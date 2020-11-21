06a1e187472264aef50a34ce493377b85cb6a93d
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2020-11-21 21:07:34 +0530

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -67,7 +67,7 @@ export default function footer() {
               alt="love"
             />
           </i>{" "}
-          By Aryan Vikash
+          By MrVishaL
         </div>
       </footer>
     </div>
