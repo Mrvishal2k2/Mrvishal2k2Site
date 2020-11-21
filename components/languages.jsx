c3206c56cee0c71396dbf33b669089dafa8ce513
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2020-11-21 21:19:57 +0530

    Update languages.jsx

--- components/languages.jsx
+++ components/languages.jsx
@@ -1,16 +1,6 @@
 
 export default function Languages() {
   return (
-    <div className="techstack">
-      <div className="lang">
-        <h1>
-          <span className="badge badge-danger">Skills</span>
-        </h1>
-      </div>
-
-      <div className="grid-flex">
-        <img src="https://img.icons8.com/fluent/240/000000/android-os.png" />
-      </div>
 
       {/* Programming Languages */}
       <div className="lang">
@@ -25,18 +15,11 @@ export default function Languages() {
       {/*  Frameworks */}
       <div className="lang">
         <h1>
-          <span className="badge badge-danger">Frameworks</span>
-        </h1>
-      </div>
-
-      <div className="lang">
-        <h1>
           <span className="badge badge-danger">Tools</span>
         </h1>
       </div>
 
       <div className="grid-flex">
-        <img src="https://img.icons8.com/color/480/000000/visual-studio-code-2019.png" />
         <img src="https://img.icons8.com/color/480/000000/intellij-idea.png" />
         <img src="https://img.icons8.com/color/144/000000/git.png" />
         <img src="https://img.icons8.com/fluent/96/000000/github.png" />
