d212279119502f94fef646ad15cb9f70a6bec5b5
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-09-28 01:54:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export default {
   githubUsername: "Mrvishal2k2",
   websiteName: "@MrVishaL2k2",
   heroHead: "Hey There !!",
-  heroText: "Hehe,MrVishaL is here , I’m 👇",
+  heroText: "MrVishaL is here, I’m 👇",
   facebookUsername: "VishaL",
   instaUsername: "mrvishal2k2",
   typedArray : ["Student","MrProGeeky","TechBlogger","Ex-Youtuber","NoobDev","Quick-Learner"]
