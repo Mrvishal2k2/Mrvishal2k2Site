9db0a238c1bc1081dd7b8050113e891065594246
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-09-28 02:25:37 +0530

    Get pushes (#1)
    * Update projects.jsx
    
    * Update projects.jsx
    
    * Delete package-lock.json
    
    * Update package.json
    
    * Update index.js
    
    * Update projects.js
    
    * Update package.json
    
    * Create yark.lock
    
    * Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 
 
 import Hero from "../components/hero"
@@ -6,12 +7,34 @@ import Footer from "../components/footer"
 
 export default function Home() {
 
-
+const description = "A self learned coder with few pro Skills"
+const pageTitle = "Mrvishal"
+const siteName = "Mrvishal2k2"
 
 
 
   return (
     <div>
+      <Head>
+      <title>{pageTitle}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="description" content="A self learned coder with few pro Skills" />
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
+        <meta name="twitter:card" content="summary_large_image" key="twcard" />
+        <meta property="og:description" content={description} key="ogdesc" />
+        <meta name="twitter:creator" content="@_aryanvikash" key="twhandle" />
+        
+
+        {/* Open Graph */}
+        <meta property="og:url" content="https://mrvishal2k2.me" key="ogurl" />
+        <meta property="og:image" content="https://GitHub.com/mrvishal2k2.png" key="ogimage" />
+        <meta property="og:site_name" content={siteName} key="ogsitename" />
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
+        <meta property="og:description" content={description} key="ogdesc" />
+                
+      </Head>
+
       <Hero />
       <Languages />
       <Footer />
